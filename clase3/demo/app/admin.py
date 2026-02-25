8d27174ac73572139488036756c718cabd3d3573
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-02-25 20:41:45 -0300

    cambio de nombre

--- clase3/demo/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-## debo importar mis modelos desde model.py
-from .models import Producto
-
-# Register your models here.
-#para registrar un modelo en el panel de administrador
-
-admin.site.register(Producto)
--- clase3/demo/app/urls.py
+++
@@ -1,8 +0,0 @@
-
-
-
-from django.urls import path
-from .views import lista_productos
-urlpatterns = [
-    path('',lista_productos, name='lista_productos' ),
-]
